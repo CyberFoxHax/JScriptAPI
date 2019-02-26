a25661c7ee5ee994effe41615de7bd8d951f444f
CyberFox Hax <cyber_fox_2@hotmail.com>
2019-02-26 18:22:24 +0800

    Update TextStream.cs
    filling api

--- TextStream.cs
+++ TextStream.cs
@@ -1,4 +1,15 @@
-class TextStream{
+class TextStream{ // http://www.devguru.com/content/technologies/vbscript/objects-textstream.html
+	bool AtEndOfLine { get; }
+	bool AtEndOfStream { get; }
+	int Column { get; set; }
+	int Line { get; set; }
+	string Read();
 	string ReadAll();
-	// void WriteAll(string text);
+	string ReadLine();
+	void Skip();
+	void SkipLine();
+	void Write(string text);
+	void WriteLine(string text);
+	void WriteBlankLines(int count);
+	void Close();
 }
