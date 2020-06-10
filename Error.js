2455c58b4982bf78060cebf58b8312bb096811dc
Jesus <joa@qitiandesign.cn>
2020-06-10 15:08:56 +0800

    adding error type

---
+++ Error.js
@@ -0,0 +1,6 @@
+class Error{ // from catch block
+	public string name { get; set; } // Error
+	public int number { get; set; }
+	public string description { get; set; }
+	public string message { get; set; }
+}
