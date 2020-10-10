024a594a4af5c274e88a4a3f1b0c45c303422a19
cyberfox <cyberfox@procrinity>
2020-10-10 23:52:45 +0800

    simplifying

--- Useful samples/enumarating files and folders.js
+++ Useful samples/enumarating files and folders.js
@@ -2,23 +2,21 @@ var shell = WScript.CreateObject("WScript.Shell");
 var fso = WScript.CreateObject("Scripting.FileSystemObject");
 
 
-function IterateCurrentDirectory(){
-	 // CurrentDirectory being the executing directory
-	 // passing a relative path will find from the executing directory
-	var folder = fso.GetFolder(shell.CurrentDirectory);
-	
-	// folders
+{ // subdirectories
+	var folder = fso.GetFolder(shell.CurrentDirectory);	
 	for(var e = new Enumerator(folder.SubFolders); e.atEnd()===false; e.moveNext()){
 		WScript.Echo(e.item());
 	}
-	
-	// files
+}
+
+{ // files
+	var folder = fso.GetFolder(shell.CurrentDirectory);	
 	for(var e = new Enumerator(folder.Files); e.atEnd()===false; e.moveNext()){
 		WScript.Echo(e.item());
 	}
 }
 
-function UsingDirCmd(){
+{ // by parsing DIR
 	var command = "dir /b";
 	var WshShell = WScript.CreateObject("WScript.Shell");
 	var res = WshShell.Exec("cmd /c " + command).StdOut.ReadAll();
